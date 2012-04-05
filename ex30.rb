50997f7ba387871848685cfeb9d55385043280c3
Kimbeaux <KimbeauxE@verizon.net>
2012-04-04 20:38:45 -0400

    ex30 w/ extra credit

---
+++ ex30.rb
@@ -0,0 +1,29 @@
+people = 30
+cars = 40
+buses = 15
+
+if cars > people
+  puts "We should take the cars."
+elsif cars < people
+  puts "We should not take the cars."
+else
+  puts "We can't decide."
+end
+
+if buses > cars
+  puts "That's too many buses."
+elsif buses < cars
+  puts "Maybe we could take the buses."
+else
+  puts "We still can't decide."
+end
+
+if people > buses
+  puts "Alright, let's just take the buses."
+else
+  puts "Fine, lets stay home then."
+end
+
+# Extra Credit
+# elsif gives a conditional statement that runs if those above it are false
+# else runs if all contidional statements above it are false.
